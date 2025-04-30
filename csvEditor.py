7cb884ecfd64a46cb7ee99a2d4eb31027d94dd28
Valentin Leinweber <valentinleinweber@w219-2h-v4.eduroam.dynamic.rbg.tum.de>
2025-04-30 10:25:18 +0200

    modified write_results_... now also writes the functions name into the file for better evaluation

--- csvEditor.py
+++ csvEditor.py
@@ -20,13 +20,13 @@ class csvEditor():
         print(f"Dataset with confidence scores saved successfully at: {file_path}")
         
     @staticmethod
-    def write_results_to_csv(file_name, model_name, mean_rank, mrr, hits_at_1, hits_at_5, hits_at_k, dataset_file, loss, epochs, embDims, batchSize, margin):
+    def write_results_to_csv(file_name, function_name, model_name, mean_rank, mrr, hits_at_1, hits_at_5, hits_at_k, dataset_file, loss, epochs, embDims, batchSize, margin):
         # Check if the file exists to write headers only once
         file_exists = os.path.exists(file_name)
 
         # Open the file in append mode ('a') to add new rows instead of overwriting
         with open(file_name, mode="a", newline="") as file:
-            fieldnames = ["Model", "Dataset", "Epochs", "Embedding Dims", "Batch Size", "Margin", "Loss", "Mean Rank", "MRR", "Hits@1", "Hits@5", "Hits@10"]
+            fieldnames = ["Model", "Dataset", "Function", "Epochs", "Embedding Dims", "Batch Size", "Margin", "Loss", "Mean Rank", "MRR", "Hits@1", "Hits@5", "Hits@10"]
             writer = csv.DictWriter(file, fieldnames=fieldnames)
 
             # Write the header only if the file doesn't already exist
@@ -37,6 +37,7 @@ class csvEditor():
             writer.writerow({
                 "Model": model_name,
                 "Dataset": dataset_file,
+                "Function": function_name,
                 "Epochs": epochs,
                 "Embedding Dims": embDims,
                 "Batch Size": batchSize,
