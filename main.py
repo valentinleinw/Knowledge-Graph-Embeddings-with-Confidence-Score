3fb199d45d46463e2df44647db25f007afa1c812
Valentin Leinweber <valentinleinweber@Valentins-MacBook-Air.local>
2025-07-17 15:29:06 +0200

    main method now ready for testing

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ if __name__ == "__main__":
         df_avg.to_csv(csv_path, index=False)
     
     triples = [
-        ("datasets/paper_bounded_CoDExSmall.csv", ds.CoDExSmall(), f"results/paper_bounded_CoDExSmall_results/evaluation_results")]
-    """("datasets/paper_logistic_CoDExSmall.csv", ds.CoDExSmall(), f"results/paper_logistic_CoDExSmall_results/evaluation_results"),
+        ("datasets/paper_bounded_CoDExSmall.csv", ds.CoDExSmall(), f"results/paper_bounded_CoDExSmall_results/evaluation_results"),
+        ("datasets/paper_logistic_CoDExSmall.csv", ds.CoDExSmall(), f"results/paper_logistic_CoDExSmall_results/evaluation_results"),
         ("datasets/CoDExSmall_agree___with_confidence.csv", ds.CoDExSmall(), f"results/CoDExSmall_agree_with_confidence_results/evaluation_results"),
         ("datasets/CoDExSmall_appearances___with_confidence.csv", ds.CoDExSmall(), f"results/CoDExSmall_appearances_with_confidence_results/evaluation_results"),
         ("datasets/CoDExSmall_average___with_confidence.csv", ds.CoDExSmall(), f"results/CoDExSmall_average_with_confidence_results/evaluation_results"),
@@ -80,14 +80,13 @@ if __name__ == "__main__":
         ("datasets/YAGO310_random_[0;0.5]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random_with_confidences_results/evaluation_results"),
         ("datasets/YAGO310_random_[0;1]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random1_with_confidences_results/evaluation_results"),
         ("datasets/YAGO310_random_[0.5;1]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random2_with_confidences_results/evaluation_results"),
-        ("datasets/YAGO310_ranked_appearances___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_ranked_appearances_with_confidence_results/evaluation_results"),
-        ]"""
+        ("datasets/YAGO310_ranked_appearances___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_ranked_appearances_with_confidence_results/evaluation_results")]
     
     for origin, dataset, result in triples:
         for i in range(3):
             train.train_and_evaluate(origin, dataset, "loss", embedding_dim=20, batch_size=48, num_epochs=10, result_file=result + "_loss.csv")
             
-            """train.train_and_evaluate(origin, dataset, "objective", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_objective.csv")
+            train.train_and_evaluate(origin, dataset, "objective", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_objective.csv")
 
             train.train_and_evaluate(origin, dataset, "divergence", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_divergence.csv")
 
@@ -100,7 +99,21 @@ if __name__ == "__main__":
             train.train_and_evaluate_neg_confidences_inverse(origin, dataset, embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_inverse.csv")
 
             train.train_and_evaluate_neg_confidences_similarity(origin, dataset, embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_similarity.csv")
-    """
+    
         avg(result + "_loss.csv")
+        
+        avg(result + "_objective.csv")
+        
+        avg(result + "_divergence.csv")
+        
+        avg(result + "_gaussian.csv")
+        
+        avg(result + "_softplus.csv")
+        
+        avg(result + "_cosukg.csv")
+        
+        avg(result + "_inverse.csv")
+        
+        avg(result + "_similarity.csv")
     
     
