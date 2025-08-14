73d611b7c29229536e3a2f9de0ac17477921a6ab
Valentin Leinweber <valentinleinweber@Valentins-MacBook-Air.local>
2025-08-14 12:17:56 +0200

    added first function for plotting

--- plotter.py
+++ plotter.py
@@ -9,7 +9,31 @@ import pandas as pd
     - multiple functions, multiple datasets, to compare the functions (only singular model I think)
 """
 
-def plot_file():
+def plot_file(file, metric, models = []):
+    
+    if len(models) <= 0:
+        models = ["TransE", "ComplEx", "DistMult"]
+        
+    models = [model + "Uncertainty" for model in models]
+    
+    df = pd.read_csv(file)
+    
+    filtered_df = df[df['Model'].isin(models)]
+
+    x = filtered_df['Model']
+    y = filtered_df[metric]
+
+    # Create bar chart
+    plt.bar(x, y)
+
+    # Add labels and title
+    plt.xlabel('Models')
+    plt.ylabel(metric)
+    plt.title()
+
+    # Show the plot
+    plt.show()
+    
     return
 
 def plot_different_datasets():
