0e1b7ed84865cda6a809140749b57e3f78e434d5
Valentin Leinweber <valentinleinweber@Valentins-MacBook-Air.local>
2025-08-14 12:00:03 +0200

    added file for plotting

---
+++ plotter.py
@@ -0,0 +1,19 @@
+import matplotlib as plt
+import csv
+import pandas as pd
+
+"""
+    should have a plotter that plots:
+    - same function, different dataset, singular and multiple models
+    - same dataset, different function, singular and multiple models
+    - multiple functions, multiple datasets, to compare the functions (only singular model I think)
+"""
+
+def plot_file():
+    return
+
+def plot_different_datasets():
+    return
+
+def plot_different_datasets_and_different_functions():
+    return 
