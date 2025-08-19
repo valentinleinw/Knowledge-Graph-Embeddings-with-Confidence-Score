e4bebf3da0cb5070a1e2053c4aecf470880fa690
Valentin Leinweber <valentinleinweber@Valentins-MacBook-Air.local>
2025-08-19 20:32:32 +0200

    changed hyperparameters to normal again

--- main.py
+++ main.py
@@ -84,20 +84,20 @@ if __name__ == "__main__":
         ("datasets/YAGO310_ranked_appearances___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_ranked_appearances_with_confidence_results/evaluation_results")]
     
     for origin, dataset, result in triples:
-        for i in range(1):
-            train.train_and_evaluate(origin, dataset, "loss", embedding_dim=2, batch_size=20, num_epochs=1, result_file=result + "_loss.csv")
+        for i in range(10):
+            train.train_and_evaluate(origin, dataset, "loss", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_loss.csv")
             
-            train.train_and_evaluate(origin, dataset, "objective", embedding_dim=2, batch_size=20, num_epochs=1, result_file=result + "_objective.csv")
+            train.train_and_evaluate(origin, dataset, "objective", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_objective.csv")
 
-            train.train_and_evaluate(origin, dataset, "divergence", embedding_dim=2, batch_size=20, num_epochs=1, result_file=result + "_divergence.csv")
+            train.train_and_evaluate(origin, dataset, "divergence", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_divergence.csv")
 
-            train.train_and_evaluate(origin, dataset, "gaussian", embedding_dim=2, batch_size=20, num_epochs=1, result_file=result + "_gaussian.csv")
+            train.train_and_evaluate(origin, dataset, "gaussian", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_gaussian.csv")
 
-            train.train_and_evaluate(origin, dataset, "softplus", embedding_dim=2, batch_size=20, num_epochs=1, result_file=result + "_softplus.csv")
+            train.train_and_evaluate(origin, dataset, "softplus", embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_softplus.csv")
             
-            train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=2, batch_size=20, num_epochs=1, result_file=result + "_cosukg.csv")
+            train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_cosukg.csv")
 
-            train.train_and_evaluate_neg_confidences_inverse(origin, dataset, embedding_dim=2, batch_size=20, num_epochs=1, result_file=result + "_inverse.csv")
+            train.train_and_evaluate_neg_confidences_inverse(origin, dataset, embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_inverse.csv")
 
             #train.train_and_evaluate_neg_confidences_similarity(origin, dataset, embedding_dim=200, batch_size=2048, num_epochs=1000, result_file=result + "_similarity.csv")
     
