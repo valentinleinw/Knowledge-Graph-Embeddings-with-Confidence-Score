c8afa4acc8667c9f9bf0b50caf9dc6878e20f7c8
Valentin Leinweber <valentinleinweber@Valentins-MacBook-Air.local>
2025-09-13 13:26:41 +0200

    only cosukg

--- main.py
+++ main.py
@@ -73,17 +73,17 @@ if __name__ == "__main__":
     dataset = ds.CoDExMedium()
     result =  "results/CoDExMedium_model_DistMult_with_confidence_results/evaluation_results"
     for i in range(10):
-        train.train_and_evaluate(origin, dataset, "loss", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_loss.csv")
+        #train.train_and_evaluate(origin, dataset, "loss", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_loss.csv")
         
-        train.train_and_evaluate(origin, dataset, "objective", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_objective.csv")
+        #train.train_and_evaluate(origin, dataset, "objective", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_objective.csv")
 
-        train.train_and_evaluate(origin, dataset, "divergence", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_divergence.csv")
+        #train.train_and_evaluate(origin, dataset, "divergence", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_divergence.csv")
 
-        train.train_and_evaluate(origin, dataset, "gaussian", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_gaussian.csv")
+        #train.train_and_evaluate(origin, dataset, "gaussian", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_gaussian.csv")
 
-        train.train_and_evaluate(origin, dataset, "softplus", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_softplus.csv")
+        #train.train_and_evaluate(origin, dataset, "softplus", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_softplus.csv")
         
-        #train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_cosukg.csv")
+        train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_cosukg.csv")
 
         #train.train_and_evaluate_neg_confidences_inverse(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_inverse.csv")
 
