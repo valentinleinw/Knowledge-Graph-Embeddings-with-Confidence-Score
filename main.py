f29402466bb2382f7081890e5920dbab2b9501e4
Valentin Leinweber <valentinleinweber@Valentins-MacBook-Air.local>
2025-09-22 09:07:09 +0200

    trying to test yago

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ if __name__ == "__main__":
     triples = [
         
         
+        ("datasets/paper_bounded_YAGO310.csv", ds.YAGO310(), f"results/paper_bounded_YAGO310_results/evaluation_results"),
+        ("datasets/paper_logistic_YAGO310.csv", ds.YAGO310(), f"results/paper_logistic_YAGO310_results/evaluation_results"),
+        ("datasets/YAGO310_agree___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_agree_with_confidence_results/evaluation_results"),
+        ("datasets/YAGO310_appearances___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_appearances_with_confidence_results/evaluation_results"),
+        #("datasets/YAGO310_average___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_average_with_confidence_results/evaluation_results"),
+        #("datasets/YAGO310_logical___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_logical_with_confidence_results/evaluation_results"),
+        ("datasets/YAGO310_logical_with_distmult___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_logical_with_distmult_with_confidence_results/evaluation_results"),
+        #("datasets/YAGO310_model__ComplEx_with_confidence.csv", ds.YAGO310(), f"results/YAGO310_model_ComplEx_with_confidence_results/evaluation_results"),
+        ("datasets/YAGO310_model__DistMult_with_confidence.csv", ds.YAGO310(), f"results/YAGO310_model_DistMult_with_confidence_results/evaluation_results"),
+        #("datasets/YAGO310_model__TransE_with_confidence.csv", ds.YAGO310(), f"results/YAGO310_model_TransE_with_confidence_results/evaluation_results"),
+        #("datasets/YAGO310_random_[0;0.5]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random_with_confidences_results/evaluation_results"),
+        ("datasets/YAGO310_random_[0;1]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random1_with_confidences_results/evaluation_results"),
+        #("datasets/YAGO310_random_[0.5;1]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random2_with_confidences_results/evaluation_results"),
+        ("datasets/YAGO310_ranked_appearances___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_ranked_appearances_with_confidence_results/evaluation_results")
+    
+        
         #("datasets/paper_bounded_CoDExSmall.csv", ds.CoDExSmall(), f"results/paper_bounded_CoDExSmall_results/evaluation_results1"),
         #("datasets/paper_logistic_CoDExSmall.csv", ds.CoDExSmall(), f"results/paper_logistic_CoDExSmall_results/evaluation_results1"),
         #("datasets/CoDExSmall_agree___with_confidence.csv", ds.CoDExSmall(), f"results/CoDExSmall_agree_with_confidence_results/evaluation_results1"),
@@ -29,7 +45,7 @@ if __name__ == "__main__":
         #("datasets/CoDExMedium_logical_with_distmult___with_confidence.csv", ds.CoDExMedium(), f"results/CoDExMedium_logical_with_distmult_with_confidence_results/evaluation_results"),
         #("datasets/CoDExMedium_model__DistMult_with_confidence.csv", ds.CoDExMedium(), f"results/CoDExMedium_model_DistMult_with_confidence_results/evaluation_results"),
         #("datasets/CoDExMedium_random_[0;0.5]__with_confidence.csv", ds.CoDExMedium(), f"results/CoDExMedium_random_with_confidences_results/evaluation_results"),
-        ("datasets/CoDExMedium_random_[0;1]__with_confidence.csv", ds.CoDExMedium(), f"results/CoDExMedium_random1_with_confidences_results/evaluation_results3"),
+        #("datasets/CoDExMedium_random_[0;1]__with_confidence.csv", ds.CoDExMedium(), f"results/CoDExMedium_random1_with_confidences_results/evaluation_results3"),
         #("datasets/CoDExMedium_random_[0.5;1]__with_confidence.csv", ds.CoDExMedium(), f"results/CoDExMedium_random2_with_confidences_results/evaluation_results"),
         #("datasets/CoDExMedium_ranked_appearances___with_confidence.csv", ds.CoDExMedium(), f"results/CoDExMedium_ranked_appearances_with_confidence_results/evaluation_results"),
         
@@ -48,24 +64,8 @@ if __name__ == "__main__":
         ("datasets/CoDExLarge_random_[0;1]__with_confidence.csv", ds.CoDExLarge(), f"results/CoDExLarge_random1_with_confidences_results/evaluation_results"),
         #("datasets/CoDExLarge_random_[0.5;1]__with_confidence.csv", ds.CoDExLarge(), f"results/CoDExLarge_random2_with_confidences_results/evaluation_results"),
         ("datasets/CoDExLarge_ranked_appearances___with_confidence.csv", ds.CoDExLarge(), f"results/CoDExLarge_ranked_appearances_with_confidence_results/evaluation_results"),
-        """
+        """]
         
-        """
-        ("datasets/paper_bounded_YAGO310.csv", ds.YAGO310(), f"results/paper_bounded_YAGO310_results/evaluation_results"),
-        ("datasets/paper_logistic_YAGO310.csv", ds.YAGO310(), f"results/paper_logistic_YAGO310_results/evaluation_results"),
-        ("datasets/YAGO310_agree___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_agree_with_confidence_results/evaluation_results"),
-        ("datasets/YAGO310_appearances___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_appearances_with_confidence_results/evaluation_results"),
-        #("datasets/YAGO310_average___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_average_with_confidence_results/evaluation_results"),
-        #("datasets/YAGO310_logical___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_logical_with_confidence_results/evaluation_results"),
-        ("datasets/YAGO310_logical_with_distmult___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_logical_with_distmult_with_confidence_results/evaluation_results"),
-        #("datasets/YAGO310_model__ComplEx_with_confidence.csv", ds.YAGO310(), f"results/YAGO310_model_ComplEx_with_confidence_results/evaluation_results"),
-        ("datasets/YAGO310_model__DistMult_with_confidence.csv", ds.YAGO310(), f"results/YAGO310_model_DistMult_with_confidence_results/evaluation_results"),
-        #("datasets/YAGO310_model__TransE_with_confidence.csv", ds.YAGO310(), f"results/YAGO310_model_TransE_with_confidence_results/evaluation_results"),
-        #("datasets/YAGO310_random_[0;0.5]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random_with_confidences_results/evaluation_results"),
-        ("datasets/YAGO310_random_[0;1]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random1_with_confidences_results/evaluation_results"),
-        #("datasets/YAGO310_random_[0.5;1]__with_confidence.csv", ds.YAGO310(), f"results/YAGO310_random2_with_confidences_results/evaluation_results"),
-        ("datasets/YAGO310_ranked_appearances___with_confidence.csv", ds.YAGO310(), f"results/YAGO310_ranked_appearances_with_confidence_results/evaluation_results")"""]
-    
     """For every dataset except the logical with distmult(?) use only the first function for evaluation, 
         so we can compare the functions themselves but also thecreation of the datasets"""
     
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     #dataset = ds.CoDExLarge()
     #result =  "results/CoDExLarge_model_DistMult_with_confidence_results/evaluation_results"
         for i in range(10):
-            #train.train_and_evaluate(origin, dataset, "loss", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_loss.csv")
+            train.train_and_evaluate(origin, dataset, "loss", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_loss.csv")
             
             #train.train_and_evaluate(origin, dataset, "objective", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_objective.csv")
 
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
             #train.train_and_evaluate(origin, dataset, "softplus", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_softplus.csv")
             
-            train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_cosukg.csv")
+            #train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_cosukg.csv")
 
             #train.train_and_evaluate_neg_confidences_inverse(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_inverse.csv")
 
