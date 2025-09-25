7bcddedfbddf5ca4e46996407c7f3036c823a9ff
Valentin Leinweber <valentinleinweber@Valentins-MacBook-Air.local>
2025-09-25 09:56:17 +0200

    trying to test yago

--- main.py
+++ main.py
@@ -80,11 +80,11 @@ if __name__ == "__main__":
 
             #train.train_and_evaluate(origin, dataset, "divergence", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_divergence.csv")
 
-            #train.train_and_evaluate(origin, dataset, "gaussian", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_gaussian.csv")
+            train.train_and_evaluate(origin, dataset, "gaussian", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_gaussian.csv")
 
             #train.train_and_evaluate(origin, dataset, "softplus", embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_softplus.csv")
             
-            train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_cosukg.csv")
+            #train.train_and_evaluate_neg_confidences_cosukg(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_cosukg.csv")
 
             #train.train_and_evaluate_neg_confidences_inverse(origin, dataset, embedding_dim=100, batch_size=1024, num_epochs=500, result_file=result + "_inverse.csv")
 
